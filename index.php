cb5ac18f788637c57fccdb3202f53dad78e37c18
GuiTufaile <guilhermetufaile@gmail.com>
2017-06-18 18:08:14 -0300

    projeto inicial

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html><html lang="pt-br" xmlns:fb="http://ogp.me/ns/fb#" xml:lang="pt" xmlns="http://www.w3.org/1999/xhtml">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-25222145-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<head>
+		<meta charset="utf-8">
+		<title>Dropbox Business</title>
+		<meta name="description" content="O Dropbox Business é a solução segura para compartilhamento e armazenamento de arquivos que já conquistou os funcionários e a confiança dos administradores de TI.">
+		<meta property="og:type" content="website"/>
+		<meta property="og:title" content="Dropbox Business - Compartilhamento e Armazenamento em nuvem"/>
+		<meta property="og:description" content="O Dropbox Business é a solução segura para compartilhamento e armazenamento de arquivos que já conquistou os funcionários e a confiança dos administradores de TI."/>
+		<meta property="og:url" content="https://www.dropbox.com/business"/>
+		<meta property="og:image" content="http://tufaile.xyz/img/icon-dropbox.svg"/>
+  
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="icon" href="img/icon-dropbox.svg" type="image/svg">
+        <link rel="shortcut" href="img/icon-dropbox.ico" type="image/x-icon">
+
+  
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/responsivo.css">
+		<script src="js/libs/modernizr.custom.45655.js"></script>
+	</head>
+	<body>
+      <!--
+        <?php/* include_once("analyticstracking.php") */?>
+      --> 
+
+		<header class="header">
+			<div class="container">
+              <div class="row">    
+                  <a href="index.php" class="col-xs-10 col-md-4 logo">                    
+                        <img src="img/icon-dropbox.svg" alt="Logo Dropbox">
+                        <img src="img/dropbox-business.svg" alt="Dropbox Business">
+                  </a>
+                  <nav class="col-md-8 header_menu">
+                      <ul>
+                          <li><a href="#recursos">Recursos</a></li>
+                          <li><a href="#quem-usa">Quem Aprova</a></li>
+                          <li><a href="#vantagens">Detalhes</a></li>                        
+                          <li><a href="form.php">Experimente</a></li>
+                      </ul>
+                  </nav>
+              </div>
+			</div>
+		</header>
+
+		<section class="introducao">
+			<div class="container">
+				<h1>Projetado para levar o trabalho em equipe a outro nível</h1>
+				<blockquote class="quote-externo">
+					<p>“O Dropbox Business é a solução segura para compartilhamento e armazenamento de arquivos que já conquistou os funcionários e a confiança dos administradores de TI.”</p>
+					<p>Compartilhe, sincronize e colabore com segurança.</p>
+				</blockquote>
+                <!-- Begin MailChimp Signup Form -->
+                <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+                <style type="text/css">
+                    #mc_embed_signup{clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+                    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                </style>
+                <div id="mc_embed_signup">
+                <form action="//tec-facil.us6.list-manage.com/subscribe/post?u=0c53f3f52748c8f0661c966fe&amp;id=7a4e4001e2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+
+                    <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Endereço de e-mail" required>
+                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0c53f3f52748c8f0661c966fe_7a4e4001e2" tabindex="-1" value=""></div>
+                      <div class="clear">
+                        <input type="submit" value="Faça uma avaliação grátis" name="subscribe" id="mc-embedded-subscribe" class="btn">
+                      </div>
+                    </div>
+                </form>
+                </div>
+                <!--End mc_embed_signup-->
+			</div>
+		</section>
+        
+        <!-- -->
+        <section class="quebra">
+			<blockquote class="quote-externo container">
+				<p>“O Dropbox Business ajuda sua empresa a crescer sem limitações, mantendo com você o controle completo sobre informações empresariais importantes e sobre a atividade dos usuários.”</p>
+			</blockquote>
+		</section>
+        
+        <section class="container recursos" data-group="recursos" id="recursos">
+          
+            <ul class="tab-menu" data-anime="scroll">
+                <li><a href="#trabalho-em-equipe" data-click="trabalho-em-equipe">Trabalho em Equipe</a></li>
+                <li><a href="#performace-controle" data-click="performace-controle">Performace e Controle</a></li>
+                <li><a href="#dropbox-paper" data-click="dropbox-paper">Dropbox Paper</a></li>
+                <li><a href="#seguro-e-confiavel" data-click="seguro-e-confiavel">Seguro e confiável</a></li>
+              
+            </ul>
+
+            <div class="item" id="trabalho-em-equipe" data-target="Trabalho em equipe">
+                <div class="item-img">
+                    <img src="img/melhor-forma-trabalhar-em-equipe.png" alt="A melhor forma de trabalhar em equipe">
+                </div>
+                <div class="item-info">
+                    <h3>A melhor forma de trabalhar em equipe</h3>
+                    <p>Mais de 500 milhões de usuários adoram o Dropbox por seus recursos de colaboração e acesso a qualquer hora, de qualquer lugar.</p>
+                    <a class="saiba-mais" href="#"> Saiba mais. </a>                  
+                </div>
+            </div>
+
+            <div class="item" id="performace-controle" data-target="Performace e Controle">
+                <div class="item-img">
+                    <img src="img/performace-controle-uso-empresarial.png" alt="performace e controle no uso empresarial">
+                </div>
+                <div class="item-info">
+                    <h3>Performance e controles prontos para uso empresarial</h3>
+                    <p>
+                      Mais de 500 milhões de usuários adoram o Dropbox por seus recursos de colaboração e acesso a qualquer hora, de qualquer lugar. 
+                    </p>
+                    <a class="saiba-mais" href="#"> Saiba mais. </a>
+                </div>
+            </div>
+          
+            <div class="item" id="dropbox-paper" data-target="Dropbox Paper">
+                <div class="item-img">
+                    <img src="img/dropbox-paper.png" alt="Dropbox Paper">
+                </div>
+                <div class="item-info">
+                    <h3>Dropbox Paper</h3>
+                    <p>
+                      Uma maneira simples e poderosa de criar, compartilhar e manter sua equipe em sincronia — com o benefício adicional dos controles administrativos para proteger sua empresa. 
+                    </p>
+                    <a class="saiba-mais" href="#"> Saiba mais. </a>
+                </div>
+            </div>
+            
+            <div class="item" id="seguro-e-confiavel" data-target="Seguro e confiável">
+                <div class="item-img">
+                    <img src="img/seguro-e-confiavel.png" alt="Seguro e confiável">
+                </div>
+                <div class="item-info">
+                    <h3>Seguro e confiável</h3>
+                    <p>
+                      Atendemos às necessidades de segurança, conformidade e privacidade de empresas como Hyatt, News Corp e National Geographic.
+                    </p>
+                    <a class="saiba-mais" href="#"> Saiba mais. </a>
+                </div>
+            </div>
+            
+
+        </section>
+      
+		<section class="portfolio">
+			<div class="container">
+				<h2 class="subtitulo">Portfólio</h2>
+				<div class="portfolio_lista">
+					<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
+					<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
+					<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
+				</div>
+				<div class="call">
+					<p>conheça mais o nosso portfólio</p>
+					<a href="portfolio.html" class="btn">Portfólio</a>
+				</div>
+			</div>
+		</section>
+            
+	<!-- JavaScript -->
+	<script src="js/libs/jquery-1.11.2.min.js"></script>
+	<script src="js/plugins.js"></script>
+	<script src="js/main.js"></script>
+	<!-- JavaScript -->
+
+	</body>
+</html>
